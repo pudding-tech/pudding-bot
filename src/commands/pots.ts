0803994a9208b2567a27f7bf9a0413c894e97f2e
MotionDuck <andreasoeium@gmail.com>
2022-07-25 16:00:42 +0200

    Fix volume command input + Small text changes for various other commands

--- src/commands/pots.ts
+++ src/commands/pots.ts
@@ -4,7 +4,7 @@ import { BOT_COLOR, Category } from "../constants";
 
 export const pots: CommandDefinition = {
   name: "pots",
-  description: "Link to Puddings of the Storm Youtube playlist",
+  description: "Link to Puddings of the Storm HotS team",
   category: Category.LINK,
   executor: async (interaction) => {
     const potsEmbed = new MessageEmbed({
@@ -19,7 +19,7 @@ export const pots: CommandDefinition = {
 };
 
 const potsMessage = "The Heroes of the Storm team that participated in the offical Heroes Lounge league, seasons 12 - 14.\n\n" +
-  "[Puddings of the Storm - Heroes Lounge profile](https://heroeslounge.gg/team/view/PUD)\n\n" +
-  "[Puddings of the Storm - Heroes Lounge Youtube playlist](https://www.youtube.com/playlist?list=PLVXb9AssBoRDlJjkS7BYz55XOtYbrMrKV)\n\n" +
-  "----------\n\n" +
-  "[Most memorable moment in PotS's history](https://youtu.be/EGEJVbvHhiA?t=526)";
+  "[Puddings of the Storm - Heroes Lounge profile](https://heroeslounge.gg/team/view/PUD)\n" +
+  "[Puddings of the Storm - Youtube playlist](https://www.youtube.com/playlist?list=PLVXb9AssBoRDlJjkS7BYz55XOtYbrMrKV)\n\n" +
+  "**Hall of Fame - Memorable Moments:**\n" +
+  "1. [Drafting the Ultimate Team [PUD vs InPanic]](https://youtu.be/EGEJVbvHhiA?t=526)";
